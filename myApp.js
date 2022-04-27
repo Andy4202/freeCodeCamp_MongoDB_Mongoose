c9396d0227d29a3e01ed79c0fb2680642e6e0325
Andy Allan <Andy4202@gmail.com>
2022-04-27 17:18:05 +0100

    Challenge 6 completed.

--- myApp.js
+++ myApp.js
@@ -44,7 +44,7 @@ const findPeopleByName = (personName, done) => {
 
 
 const findOneByFood = (food, done) => {
-  Person.findOne({favouriteFoods: food}, function(err, personFound){
+  Person.findOne({favoriteFoods: food}, function(err, personFound){
     if (err) return console.log(err);
     done(null, personFound);
   })
