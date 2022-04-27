aa9ba96cdc43504eeb6c40dba08d21f0bd9c8d54
Andy Allan <Andy4202@gmail.com>
2022-04-27 13:43:12 +0100

    Challenge 2 completed. personScheme created and Person model created from the personSchema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,16 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const Schema  = mongoose.Schema;
+
+const personSchema = new Schema({
+  name: {type: String, required: true},
+  age: Number,
+  favoriteFoods: [String]
+});
+
+const Person = mongoose.model("Person", personSchema);
+console.log('Person: ', Person);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
